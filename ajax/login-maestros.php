f1b7c5e11cf68d777a66c468942fb519751b92af
marco2289 <mm_1989@hotmail.es>
2018-12-16 11:39:37 -0600

    ajax
    loggin

---
+++ ajax/login-maestros.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+    $archivo = fopen("../../Formularios-Administracion/data/registro_docente.json","r");
+    while(($linea=fgets($archivo))){
+        $registro=json_decode($linea,true);
+        if(
+            $_POST["No_Cuenta"]==$registro["No_Cuenta"]&&
+            $_POST["password"]==$registro["password"] && $registro["jerarquia"]=="Catedratico"
+        ){
+            $registro["estatus"]="1";
+            $registro["mensaje"]="Acceso autorizado";
+            $_SESSION["No_Cuenta"]=$_POST["No_Cuenta"];
+            $_SESSION["jerarquia"]=$registro["jerarquia"];
+            echo json_encode($registro);
+            exit;
+        }
+    }
+    $registro = array();
+    $registro["estatus"] = "0";
+    $registro["mensaje"] = "Credendiales Invalidas, Por Favor Introduzca las nuevas Credenciales y Asegurese de que esten Correctas";
+    echo json_encode($registro);
+?>
